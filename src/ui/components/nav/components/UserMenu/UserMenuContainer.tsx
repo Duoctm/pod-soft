c605e005ae84536a3615561cda7dd7e6e95523ba
Duoctm <Duoctm@podsoft.com.vn>
2025-05-10 18:05:10 +0700

    deploy 10-05-2025/18:05

--- src/ui/components/nav/components/UserMenu/UserMenuContainer.tsx
+++ src/ui/components/nav/components/UserMenu/UserMenuContainer.tsx
@@ -9,21 +9,31 @@ import { UserDetailsFragment } from "@/gql/graphql";
 
 export function UserMenuContainer() {
 	const [user, setUser] = useState<UserDetailsFragment>();
+	const [isLoading, setIsLoading] = useState(false);
 
 	useEffect(() => {
 		const fetchUser = async () => {
+			setIsLoading(true);
 			try {
 				const data = await getUser();
 				setUser(data as UserDetailsFragment);
 			} catch (e) {
 				console.error("Failed to fetch user", e);
+			} finally {
+				setIsLoading(false);
 			}
 		};
 		fetchUser();
 	}, []);
 
-	if (!user) return <div className="animate-spin rounded-full h-6 w-6 border-t-2 border-b-2 border-gray-900" />;
-	if (user) return <UserMenu user={user} />;
+	if (isLoading) {
+		return <div className="animate-spin rounded-full h-6 w-6 border-t-2 border-b-2 border-gray-900" />;
+	}
+
+	if (user) {
+		return <UserMenu user={user} />;
+	}
+
 	return (
 		<LinkWithChannel href="/login" className="flex items-center justify-center rounded-md p-2">
 			<UserIcon className="h-6 w-6" aria-hidden="true" />
