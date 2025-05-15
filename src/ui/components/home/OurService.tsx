0d50e83555ec31024a8a304dd649b90ce9a76bd6
Duoctm <Duoctm@podsoft.com.vn>
2025-05-15 19:31:06 +0700

    deploy 15-05-2025/19:31

--- src/ui/components/home/OurService.tsx
+++ src/ui/components/home/OurService.tsx
@@ -19,30 +19,30 @@ const SERVICES = [
 		link: "/default-channel/service",
 	},
 	{
-		icon: <Image />,
-		title: "Embroidery",
-		description:
-			"Single headed machines for custom orders. Dedicated quality embroidery staff. Tajima machines with 15 thread colors.",
-		link: "/default-channel/service",
-	},
-	{
 		icon: <Layout />,
 		title: "Hard Goods",
 		description: "Premium drinkware options. Mimaki and Grando machines. Professional finishing.",
 		link: "/default-channel/service",
 	},
-	{
-		icon: <FileText />,
-		title: "Custom Boxes",
-		description: "Eye-catching designs. Perfect for executive kits. Premium packaging solutions.",
-		link: "/default-channel/service",
-	},
-	{
-		icon: <Package />,
-		title: "Canvas Print",
-		description: "Latex HP printers. Vibrant colors. Durable materials.",
-		link: "/default-channel/service",
-	},
+	// {
+	// 	icon: <Image />,
+	// 	title: "Embroidery",
+	// 	description:
+	// 		"Single headed machines for custom orders. Dedicated quality embroidery staff. Tajima machines with 15 thread colors.",
+	// 	link: "/default-channel/service",
+	// },
+	// {
+	// 	icon: <FileText />,
+	// 	title: "Custom Boxes",
+	// 	description: "Eye-catching designs. Perfect for executive kits. Premium packaging solutions.",
+	// 	link: "/default-channel/service",
+	// },
+	// {
+	// 	icon: <Package />,
+	// 	title: "Canvas Print",
+	// 	description: "Latex HP printers. Vibrant colors. Durable materials.",
+	// 	link: "/default-channel/service",
+	// },
 ];
 
 interface ServiceCardProps {
