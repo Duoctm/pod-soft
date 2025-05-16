10a825a172ab11a37add0c10b47462fad86b4f20
Duoctm <Duoctm@podsoft.com.vn>
2025-05-16 10:05:21 +0700

    deploy 16-05-2025/10:05

--- src/ui/components/ScrollToTop.tsx
+++ src/ui/components/ScrollToTop.tsx
@@ -1,18 +1,19 @@
-'use client';
+"use client";
 
-import { useEffect } from 'react';
-import { usePathname } from 'next/navigation';
+import { useEffect } from "react";
+import { usePathname } from "next/navigation";
 
 export default function ScrollToTop() {
   const pathname = usePathname();
-  console.log(pathname)
+
   useEffect(() => {
-
-    window.scrollTo({
-      top: 0,
-      behavior: 'smooth'
-    })
-
+    setTimeout(() => {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+      }
+      );
+    }, 100); // delay để đảm bảo DOM render xong
   }, [pathname]);
 
   return null;
