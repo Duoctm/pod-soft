f97d988c9bf621b210b5f3cc3715853935785831
Duoctm <Duoctm@podsoft.com.vn>
2025-05-19 17:31:57 +0700

    deploy 19-05-2025/17:31

--- src/app/[channel]/layout.tsx
+++ src/app/[channel]/layout.tsx
@@ -2,6 +2,36 @@ import { type ReactNode } from "react";
 import { executeGraphQL } from "@/lib/graphql";
 import { ChannelsListDocument } from "@/gql/graphql";
 
+// app/default-channel/page.tsx
+
+export const metadata = {
+	title: "ZoomPrint",
+	description: "ZoomPrints is your gateway to rapid fast fulfillment minus the steep investment.",
+	openGraph: {
+		title: "ZoomPrint",
+		description: "ZoomPrints is your gateway to rapid fast fulfillment minus the steep investment.",
+		url: "../../",
+		images: [
+			{
+				url: "./opengraph-image.png",
+				width: 1200,
+				height: 630,
+				alt: "ZoomPrint - In nhanh",
+			},
+		],
+	},
+};
+
+export default function DefaultChannelPage() {
+	return (
+		<main>
+			<h1>Default Channel</h1>
+		</main>
+	);
+}
+
+
+
 export const generateStaticParams = async () => {
 	// the `channels` query is protected
 	// you can either hardcode the channels or use an app token to fetch the channel list here
