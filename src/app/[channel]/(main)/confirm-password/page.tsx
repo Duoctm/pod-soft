c50fbf9cd764448bba9d39e09b1bf93e7c46798e
Duoctm <Duoctm@podsoft.com.vn>
2025-05-23 17:18:12 +0700

    deploy 23-05-2025/17:18

--- src/app/[channel]/(main)/confirm-password/page.tsx
+++ src/app/[channel]/(main)/confirm-password/page.tsx
@@ -11,7 +11,12 @@ import { setPasswordOnServer } from "./actions/server";
 import "react-toastify/dist/ReactToastify.css"; // Import the CSS
 
 const validationSchema = Yup.object({
-	password: Yup.string().min(8, "Password must be at least 8 characters").required("Password is required"),
+	password: Yup.string()
+		.min(8, "Password must be at least 8 characters")
+		.matches(/[A-Z]/, "Password must contain at least one uppercase letter")
+		.matches(/[a-z]/, "Password must contain at least one lowercase letter")
+		.matches(/[0-9]/, "Password must contain at least one number")
+		.required("Password is required"),
 	confirmPassword: Yup.string()
 		.oneOf([Yup.ref("password")], "Passwords must match")
 		.required("Confirm password is required"),
