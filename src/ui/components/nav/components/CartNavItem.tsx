c12b5a85354ef496fd6ba4a4154b370ac5e46383
Duoctm <Duoctm@podsoft.com.vn>
2025-05-30 15:14:17 +0700

    deploy 30-05-2025/15:14

--- src/ui/components/nav/components/CartNavItem.tsx
+++ src/ui/components/nav/components/CartNavItem.tsx
@@ -15,14 +15,15 @@ export const CartNavItem = async ({ channel }: { channel: string }) => {
 			{lineCount > 0 ? (
 				<div
 					className={clsx(
-						"absolute bottom-0 right-0 -mb-2 -mr-2 flex h-4 flex-col items-center justify-center rounded bg-neutral-900 px-1 text-xs font-medium text-white",
+						"absolute bottom-0 right-0 -mb-2 -mr-2 h-4 min-w-[16px] px-1 rounded bg-neutral-900 text-xs font-medium text-white flex items-center justify-center transition-opacity",
+						lineCount ? "opacity-100" : "opacity-0"
 					)}
 				>
 					{lineCount > 99 ? "99+" : lineCount}
 					<span className="sr-only">item{lineCount > 1 ? "s" : ""} in cart, view bag</span>
 				</div>
 			) : (
-				<span className="sr-only">0 items in cart</span>
+				null
 			)}
 		</LinkWithChannel>
 	);
