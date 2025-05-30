13463ebd31377fd71b930178105fe7c45dfe765e
Duoctm <Duoctm@podsoft.com.vn>
2025-05-30 15:42:06 +0700

    deploy 30-05-2025/15:41

--- src/ui/components/nav/components/CartNavItem.tsx
+++ src/ui/components/nav/components/CartNavItem.tsx
@@ -15,15 +15,14 @@ export const CartNavItem = async ({ channel }: { channel: string }) => {
 			{lineCount > 0 ? (
 				<div
 					className={clsx(
-						"absolute bottom-0 right-0 -mb-2 -mr-2 h-4 min-w-[16px] px-1 rounded bg-neutral-900 text-xs font-medium text-white flex items-center justify-center transition-opacity",
-						lineCount ? "opacity-100" : "opacity-0"
+						"absolute bottom-0 right-0 -mb-2 -mr-2 flex h-4 flex-col items-center justify-center rounded bg-neutral-900 px-1 text-xs font-medium text-white",
 					)}
 				>
 					{lineCount > 99 ? "99+" : lineCount}
 					<span className="sr-only">item{lineCount > 1 ? "s" : ""} in cart, view bag</span>
 				</div>
 			) : (
-				null
+				<span className="sr-only">0 items in cart</span>
 			)}
 		</LinkWithChannel>
 	);
--- src/ui/components/wrapper.tsx
+++ src/ui/components/wrapper.tsx
@@ -3,7 +3,7 @@ import { cn } from '@/lib/utils'
 
 const Wrapper = ({ children, className, ...prev }: { children: React.ReactNode, className?: string }) => {
   return (
-    <div className={cn("max-w-[1200px] w-full mx-auto px-4 min-h-[100px] lg:min-h-[300px]", className)} {...prev}>
+    <div className={cn("max-w-[1200px] w-full mx-auto px-4", className)} {...prev}>
       {children}
     </div>
   )
