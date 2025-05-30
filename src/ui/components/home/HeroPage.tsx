d3ad6e16c20442dc999830f0ba57b7f2452ea76a
Duoctm <Duoctm@podsoft.com.vn>
2025-05-30 17:31:46 +0700

    deploy 30-05-2025/17:31

--- src/ui/components/home/HeroPage.tsx
+++ src/ui/components/home/HeroPage.tsx
@@ -5,11 +5,11 @@ import HeroFeature from "../HeroFeature";
 
 const LeftSideHero = () => {
 	return (
-		<div className="flex flex-1 flex-col text-[#424255] md:px-4 lg:px-0 items-stretch h-full min-h-[180px]">
+		<div className="flex flex-1 flex-col text-[#424255] md:px-4 lg:px-0 items-stretch h-full min-h-[100px]">
 			<h1 className="max-w-[582px] text-4xl font-bold leading-[110%] md:mt-6 md:text-6xl lg:text-7xl">
 				Welcome to ZoomPrints
 			</h1>
-			<span className="min-h-[80px] mt-4 text-lg w-full font-bold leading-[140%] md:mt-6 md:text-[32px]">
+			<span className="min-h-[40px] mt-4 text-lg w-full font-bold leading-[140%] md:mt-6 md:text-[32px]">
 				Digital printing for the promotional product industry.
 			</span>
 			{/* <p className="mt-4 w-full max-w-[546px] text-base font-normal leading-[1.6] md:mt-7 md:text-[20px]">
@@ -85,7 +85,7 @@ const HeroPage = () => {
 				<div className="flex-[1_1_50%]">
 					<LeftSideHero />
 				</div>
-				<div className="flex-[1_1_50%] flex justify-end">
+				<div className="flex-[1_1_50%] flex justify-end mt-4 md:mt-0">
 					<RightSideHero />
 				</div>
 			</div>
