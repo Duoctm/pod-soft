e12caa64a55e611df2c0edd4adb075731ed600b1
Duoctm <Duoctm@podsoft.com.vn>
2025-06-13 18:42:57 +0700

    deploy 13-06-2025:18-42

--- src/app/[channel]/(main)/design/[...slug]/utils/getProductDetailForDesign.tsx
+++ src/app/[channel]/(main)/design/[...slug]/utils/getProductDetailForDesign.tsx
@@ -4,6 +4,7 @@ import { GetProductDetailForDesignDocument } from "@/gql/graphql";
 import { executeGraphQL } from "@/lib/graphql";
 
 export const fetchRawProductDetail = async (productId: string) => {
-    const rawData = await executeGraphQL(GetProductDetailForDesignDocument, { variables: { channel: 'default-channel', id: productId } });
+    const rawData = await executeGraphQL(GetProductDetailForDesignDocument, { variables: { channel: 'us', id: productId } });
+    // const rawData = await executeGraphQL(GetProductDetailForDesignDocument, { variables: { channel: 'default-channel', id: productId } }); 
     return rawData;
 }
