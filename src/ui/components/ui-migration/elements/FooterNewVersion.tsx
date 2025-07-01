66bc2ec4133fa0dcf493c8952450802a97a2474f
Duoctm <Duoctm@podsoft.com.vn>
2025-07-01 17:25:06 +0700

    refactor: Clean up Hero and Footer components by removing unused code and adjusting layout

--- src/ui/components/ui-migration/elements/FooterNewVersion.tsx
+++ src/ui/components/ui-migration/elements/FooterNewVersion.tsx
@@ -9,16 +9,16 @@ const FooterNewVersion = () => {
         <div className='min-h-[40vh] flex flex-col items-center justify-center px-4 md:px-0 py-8 md:py-10 lg:py-14 bg-[#1E2737] relative'>
             <Image src='/images/aps-ketches.webp' fill className='object-fill opacity-20 z-10 bg-repeat-y' alt='' />
             <Wrapper className='flex flex-1 items-start md:items-center justify-between flex-col md:flex-row '>
-                <div className='flex items-start gap-4 flex-col flex-1 w-full'>
+                <div className='flex items-start gap-4 flex-col'>
                     <Link href="/">
                         <Image src="/images/logo-new-v.webp" alt="SwiftPod Logo" width={150} height={75} />
                     </Link>
-                    <span className='flex items-center gap-2 text-white text-sm justify-start w-full '>
+                    <span className='flex items-center gap-2 text-white text-sm justify-center w-full '>
                         <Link href="/"><Image src="/icons/fb.svg" alt="Facebook Logo" width={24} height={24} /></Link>
                         <Link href="/"><Image src="/icons/linkin.svg" alt="LinkedIn Logo" width={24} height={24} /></Link>
                     </span>
                 </div>
-                <div className='text-white text-sm flex flex-row md:flex-row gap-2 md:gap-4 mt-4 md:mt-0'>
+                <div className='text-white text-sm flex flex-row md:flex-row gap-2 md:gap-4 mt-4 md:mt-0 flex-1 justify-end'>
                     <div className='flex flex-col items-start gap-2'>
                         <span className='font-semibold text-xl md:text-[20px]  text-[#F98D62]'>Contact Us</span>
                         <Link href="/"><span className='text-[#FAFAFA] xl:text-[15px]'>Customer Service 408-409-5249</span></Link>
--- src/ui/components/ui-migration/home/OurMission.tsx
+++ src/ui/components/ui-migration/home/OurMission.tsx
@@ -17,10 +17,8 @@ const OurMission = () => {
                     <p className='text-sm'>
                         *Orders of 144 and lower single side print.
                     </p>
-
-
                 </div>
-                <div className='grid grid-cols-1 md:grid-cols-2 gap-4 lg:gap-8 lg:mt-8 min-h-0 md:min-h-[261px] h-full'>
+                <div className='grid grid-cols-1 md:grid-cols-2 gap-4 lg:gap-8 lg:mt-4 min-h-0 md:min-h-[261px] h-full'>
                     {/* IMAGE BLOCK */}
                     <div className='relative w-full aspect-[16/9] md:aspect-auto md:h-auto'>
                         <Image
