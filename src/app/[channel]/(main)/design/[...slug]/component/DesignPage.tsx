f8f05ec56abcb6bba958fec98d68e7dedcded61f
Duoctm <Duoctm@podsoft.com.vn>
2025-07-20 21:47:04 +0700

    Update DesignPage.tsx

--- src/app/[channel]/(main)/design/[...slug]/component/DesignPage.tsx
+++ src/app/[channel]/(main)/design/[...slug]/component/DesignPage.tsx
@@ -3663,7 +3663,7 @@ function DesignPage(param: DesignPageProps) {
 
 
 
-      <div className="fixed bottom-0 right-0 block  md:hidden">
+      <div className="fixed bottom-0 right-0 block  lg:hidden">
         {(param.typeDesign == 1) && (
           <Button
             className="flex h-12 w-12  items-center justify-center rounded-full bg-[#2c344b] p-2"
